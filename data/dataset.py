062ebe936ca0fd548e12f464c5a00ed23d7b04ec
Danil Kostromin <danil1kostr@gmail.com>
2025-09-13 16:13:05 +0300

    updating code

--- data/dataset.py
+++ data/dataset.py
@@ -1,7 +1,7 @@
 from datasets import load_dataset
 
 
-def add_tags(example):
+def _add_tags(example):
     cot_part, answer_part = example['answer'].split('####')
     example['question'] = 'Question: ' + example['question'] + 'Answer: <think>'
     example['answer'] = cot_part.strip() + '</think>' + ' <answer>####' + answer_part + '</answer>'
@@ -11,6 +11,6 @@ def add_tags(example):
 
 def get_sft_dataset():
     dataset = load_dataset('openai/gsm8k', 'main')
-    tagged_dataset = dataset.map(add_tags)
+    tagged_dataset = dataset.map(_add_tags)
 
     return tagged_dataset
